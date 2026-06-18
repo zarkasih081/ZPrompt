bf54bd48e9f0f867b43be50cba2411e23ffa702e
Zarkasih <zarkasih081@github.com>
2026-06-18 07:22:26 +0700

    Copywriting: Catch remaining lowercase 'p' in Zprompt placeholder

--- script.js
+++ script.js
@@ -322,7 +322,7 @@ function renderHistory() {
         <div class="history-item-time">${item.time}</div>
       </div>
       <div class="history-item-desc">${item.desc}</div>
-      <button class="delete-history" aria-label="Hapus history" data-id="${item.id}" title="Hapus">
+      <button class="delete-history" aria-label="Hapus riwayat" data-id="${item.id}" title="Hapus">
         <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
           <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -546,7 +546,7 @@ $('historyToggle').addEventListener('click', toggleHistory);
 $('closeHistory').addEventListener('click', toggleHistory);
 historyOverlay.addEventListener('click', toggleHistory);
 $('clearAllHistory').addEventListener('click', () => {
-  if (confirm('Yakin ingin menghapus semua history?')) {
+  if (confirm('Yakin ingin menghapus semua riwayat?')) {
     try { localStorage.removeItem('zprompt_history'); } catch(e) {}
     renderHistory();
   }
